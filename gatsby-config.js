f375e0e89860ae90620b2a33f5da15ea208e6fc9
Kevin Scott <kevin@hitheory.com>
2018-01-07 16:42:39 -0500

    Add posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,21 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    'gatsby-plugin-catch-links',
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [] // just in case those previously mentioned remark plugins sound cool :)
+      }
+    },
+  ],
 }
