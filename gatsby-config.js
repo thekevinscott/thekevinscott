b4defe2672115c7a35807f8d3f613cc08d57eeeb
Kevin Scott <kevin@hitheory.com>
2018-03-22 10:03:28 -0400

    Remove old files and update site description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Essays by Kevin Scott`,
-    description: "Thoughts about Design & Artificial Intelligence",
+    title: "Essays by Kevin Scott",
+    description: "Design meets Artificial Intelligence.",
     keywords: "Artificial Intelligence, Design, Pytorch, TensorFlow, Deep Learning, Kaggle",
     author: "Kevin Scott",
     url: "https://thekevinscott.com",
