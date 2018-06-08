3a3daa68456f661064a0c7b2e6eeb34d7cbe77f8
Kevin Scott <kevin@hitheory.com>
2018-06-08 15:49:33 -0400

    Working test suite

--- scripts/test/index.js
+++ scripts/test/index.js
@@ -2,14 +2,14 @@
 
 const spawn = require('../utils/spawn');
 const jest = require('jest');
-const PORT = '6041';
+const yargs = require('yargs').argv;
 
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'test';
 process.env.NODE_ENV = 'test';
 process.env.PUBLIC_URL = '';
 
-spawn('gatsby', ['serve', '-p', PORT]);
+spawn('gatsby', ['serve', '-p', yargs.PORT]);
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
@@ -18,11 +18,6 @@ process.on('unhandledRejection', err => {
   throw err;
 });
 
-let argv = process.argv.slice(2);
-
-// Watch unless on CI or in coverage mode
-if (false && !process.env.CI && argv.indexOf('--coverage') < 0) {
-  argv.push('--watch');
-}
-
-jest.run(argv);
+jest.run([
+  yargs.watch ? "--watch" : null,
+].filter(f => f));
--- scripts/utils/spawn.js
+++ scripts/utils/spawn.js
@@ -2,20 +2,27 @@ const spawn = require('child_process').spawn;
 
 const children = [];
 
-process.on('exit', () => {
+const killChildren = () => {
   // console.log('killing', children.length, 'child processes');
-  children.forEach(child => child.kill());
+  return children.forEach(child => child.kill());
+};
+
+process.on('exit', () => {
+  killChildren();
 });
 
 const cleanExit = () => process.exit();
 process.on('SIGINT', cleanExit); // catch ctrl-c
 process.on('SIGTERM', cleanExit); // catch kill
 
-module.exports = (...args) => {
-  const command = spawn(...args, { stdio: 'inherit' });
+module.exports = (cmd, args, opts) => {
+  const command = spawn(cmd, args, opts);
   command.on('error', function(err) {
     console.error(err);
     process.exit(1);
   });
+
   children.push(command);
 };
+
+module.exports.exit = killChildren;
