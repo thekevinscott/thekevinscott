54a56ad7143b60dbb1176bb189eb38a252109d02
Kevin Scott <kevin@hitheory.com>
2018-07-20 19:13:53 -0400

    Update date

--- src/components/SubscribeForm/config.js
+++ src/components/SubscribeForm/config.js
@@ -1,5 +1,7 @@
-export const DEEP_LEARNING_JOURNAL = `DEEP_LEARNING_JOURNAL`;
-export const TENSORFLOWJS = `TENSORFLOWJS`;
+export const DEEP_LEARNING_JOURNAL = 'DEEP_LEARNING_JOURNAL';
+export const TENSORFLOWJS = 'TENSORFLOWJS';
+export const LEAD_MAGNET_DATASET = 'LEAD_MAGNET_DATASET';
+export const HEADER_FORM = 'HEADER_FORM';
 const config = {
   [DEEP_LEARNING_JOURNAL]: {
     formID: "242548946",
@@ -40,6 +42,19 @@ const config = {
       description,
     };
   },
+  [HEADER_FORM]: (payload) => {
+    const descriptions = [
+      "I send a newsletter about design, AI and other cool stuff in 2018.",
+      "If you love AI, JavaScript, and all things web, you'll feel right at home. I'm journaling my progress learning artificial intelligence after ten years of experience building web and mobile apps for brands like Venmo and GE Healthcare.",
+    ];
+
+    return {
+      formID: "277152125",
+      headline: "My newsletter",
+      descriptionID: `${descriptionID}`,
+      description,
+    };
+  }
 };
 
 const getRand = (id, arr) => {
