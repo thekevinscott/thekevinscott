fffb2f71cd9bb74293644efcdb162ca473088100
Kevin Scott <kevin@hitheory.com>
2018-08-16 12:21:57 -0400

    Update copy images path

--- scripts/publish/common/processContent.js
+++ scripts/publish/common/processContent.js
@@ -125,11 +125,15 @@ const getImages = (filePath) => content => {
         src,
       } = parseImg(line);
 
+      console.log('src', src);
+
       const path = [
         ROOT,
         ...filePath.split("/").slice(0, -1),
         src.split("/").pop(),
       ].join("/");
+
+      console.log('path', path);
 
       return `![${alt}](${path} ${title})`;
     }
