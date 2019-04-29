adf923fbd26d0e52e4e77679f2013793c0f083d5
Kevin Scott <kevin@hitheory.com>
2019-04-29 16:25:30 -0400

    Add a form source attribute

--- src/components/SubscribeForm/index.tsx
+++ src/components/SubscribeForm/index.tsx
@@ -144,6 +144,7 @@ class SubscribeForm extends Component<ISubscribeFormProps> {
             },
             ...writeSubscriberTags({
               ...subscriberTags,
+              formSource: 'web',
               descriptionID,
             }),
           ]}
