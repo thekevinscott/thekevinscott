cf5de6428be6c2c3ebcf23d1566a481f68f0fee9
Kevin Scott <kevin@hitheory.com>
2020-09-09 09:41:41 -0400

    Update start script, but will need to figure out how to do baseURL

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 # docker build -t hugo -f ./scripts/Dockerfile ./scripts
-docker build \
--t hugo \
---build-arg USER_ID=$(id -u ${USER}) \
---build-arg GROUP_ID=$(id -g ${USER}) \
--f ./scripts/Dockerfile \
-./scripts || exit 1;
+# docker build \
+# -t hugo \
+# --build-arg USER_ID=$(id -u ${USER}) \
+# --build-arg GROUP_ID=$(id -g ${USER}) \
+# -f ./scripts/Dockerfile \
+# ./scripts || exit 1;
 
-docker run --rm -it \
-  -v $(pwd):/src \
+# docker run --rm -it \
+#   -v $(pwd):/src \
+#   -p 1313:1313 \
+#   hugo hugo server -D bind=0.0.0.0 --baseURL=http://0.0.0.0:1313
+
+docker run \
+  --rm -it \
   -p 1313:1313 \
-  hugo hugo server -D
+  -v $(pwd):/src \
+  klakegg/hugo:0.74.3-ext-alpine \
+  --cleanDestinationDir \
+  --ignoreCache \
+  --verbose \
+  server --bind=0.0.0.0 --baseURL='http://tower2.hopto.org:1313'
